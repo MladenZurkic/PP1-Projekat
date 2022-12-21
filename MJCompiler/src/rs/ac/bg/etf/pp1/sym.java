71be9d3a5c873a39bb21e1eca8926c24faad010c
Mladen Žurkić <mladen.zurkic00@gmail.com>
2022-12-21 03:22:27 +0100

    I - Lexical Analysis Done
    - Ant auto-removing all currently unnecessary classes and files
    - Added all terminals in sym.java
    - Edit mjelexer.flex
    - Created build.xml
    - Added new program for testing

--- MJCompiler/src/rs/ac/bg/etf/pp1/sym.java
+++ MJCompiler/src/rs/ac/bg/etf/pp1/sym.java
@@ -9,23 +9,64 @@ package rs.ac.bg.etf.pp1;
 /** CUP generated class containing symbol constants. */
 public class sym {
   /* terminals */
-  public static final int ELSE = 17;
-  public static final int IDENT = 15;
-  public static final int PLUS = 11;
-  public static final int RBRACE = 4;
-  public static final int RPAREN = 7;
-  public static final int EQUAL = 12;
-  public static final int SEMI = 5;
-  public static final int RETURN = 10;
-  public static final int IF = 16;
-  public static final int LPAREN = 6;
-  public static final int LBRACE = 3;
-  public static final int COMMA = 8;
-  public static final int EOF = 0;
-  public static final int NUMBER = 14;
-  public static final int PROG = 2;
-  public static final int PRINT = 9;
-  public static final int error = 1;
-  public static final int VOID = 13;
+	
+
+	public static final int EOF = 0;
+	public static final int error = 1;
+	public static final int PROG = 2;
+	public static final int BREAK = 3;
+	public static final int CLASS = 4;
+	public static final int ENUM = 5;
+	public static final int ELSE = 6;
+	public static final int CONST = 7;
+	public static final int IF = 8;
+	public static final int DO = 9;
+	public static final int WHILE = 10;
+	public static final int NEW = 11;
+	public static final int PRINT = 12;
+	public static final int READ = 13;
+	public static final int RETURN = 14;
+	public static final int VOID = 15;
+	public static final int EXTENDS = 16;
+	public static final int CONTINUE = 17;
+	public static final int THIS = 18;
+	public static final int FOREACH = 19;
+	
+	public static final int IDENT = 20;
+	
+	public static final int NUMBER = 21;
+	public static final int CHARCONST = 22;
+	public static final int BOOLCONST = 23;
+	
+	public static final int PLUS = 24;
+	public static final int MINUS = 25;
+	public static final int MUL = 26;
+	public static final int DIV = 27;
+	public static final int MOD = 28;
+	public static final int EQ = 29;
+	public static final int NEQ = 30;
+	public static final int GT = 31;
+	public static final int GEQ = 32;
+	public static final int LT = 33;
+	public static final int LEQ = 34;
+	public static final int BITAND = 35;
+	public static final int BITOR = 36;
+	public static final int EQUAL = 37;
+	public static final int INC = 38;
+	public static final int DEC = 39;
+	public static final int SEMI = 40;
+	public static final int COLON = 41;
+	public static final int COMMA = 42;
+	public static final int DOT = 43;
+	public static final int LPAREN = 44;
+	public static final int RPAREN = 45;
+	public static final int LANGLE = 46;
+	public static final int RANGLE = 47;
+	public static final int LBRACE = 48;
+	public static final int RBRACE = 49;
+	public static final int LAMBDAOP = 50;
+	
+	
+
 }
 
